36cc4a4c0b9136c10c919fa100d2a55429bf49b5
yulianikiforova1705-netizen <yulianikiforova1705@gmail.com>
2026-03-31 00:47:28 +0300

    feat: настроена маршрутизация для экрана гостя

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
 import CoreModule from './components/CoreModule';
+import { CustomerDisplay } from './components/CustomerDisplay'; // Импортируем наш новый экран
 
 function App() {
+  // Получаем текущий путь из адресной строки браузера
+  const currentPath = window.location.pathname;
+
+  // Если открыта ссылка экрана гостя, рендерим только его
+  if (currentPath === '/customer-display') {
+    return (
+      <div style={{ margin: 0, padding: 0 }}>
+        <CustomerDisplay />
+      </div>
+    );
+  }
+
+  // Во всех остальных случаях (главная страница и т.д.) загружаем основной модуль
   return (
     <div style={{ margin: 0, padding: 0 }}>
       <CoreModule />
     </div>
-  )
+  );
 }
 
 export default App;
